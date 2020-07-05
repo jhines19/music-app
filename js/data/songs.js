bcc7cfa578f1a9688cfd5aee0717ab8291320879
Jhines <jhines19@gmail.com>
2020-07-05 15:54:08 -0400

    First commit

---
+++ js/data/songs.js
@@ -0,0 +1,85 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/days like this.mp3",
+    "time": "02:49",
+    "title": "Days Like This (feat. Khalid)",
+    "artist": "GoldLink, Khalid"
+  },
+  {
+    "id": "2",
+    "url": "../../music/know my rights.mp3",
+    "time": "03:15",
+    "title": "Know My Rights (feat. Lil Baby)",
+    "artist": "6LACK, Lil Baby"
+  },
+  {
+    "id": "3",
+    "url": "../../music/sweeter.mp3",
+    "time": "02:49",
+    "title": "Sweeter (feat. Terrace Martin)",
+    "artist": "Leon Bridges, Terrace Martin"
+  },
+  {
+    "id": "4",
+    "url": "../../music/deep end.mp3",
+    "time": "02:40",
+    "title": "Deep End",
+    "artist": "Lecrae"
+  },
+  {
+    "id": "5",
+    "url": "../../music/cashis.mp3",
+    "time": "02:55",
+    "title": "Cashis",
+    "artist": "Eshon Burgundy, Yawdy Quan"
+  },
+  {
+    "id": "6",
+    "url": "../../music/ambition.mp3",
+    "time": "05:03",
+    "title": "Ambition (feat. Meek Mill & Rick Ross)",
+    "artist": "Wale, Meek Mill, Rick Ross"
+  },
+  {
+    "id": "7",
+    "url": "../../music/bloodfire.mp3",
+    "time": "03:50",
+    "title": "Bloodfire",
+    "artist": "Eshon Burgundy, Steven Malcolm"
+  },
+  {
+    "id": "8",
+    "url": "../../music/dedication.mp3",
+    "time": "04:06",
+    "title": "Dedication (feat. Kendrick Lamar)",
+    "artist": "Nipsey Hussle, Kendrick Lamar"
+  },
+  {
+    "id": "9",
+    "url": "../../music/kung fu.mp3",
+    "time": "02:36",
+    "title": "Kung Fu",
+    "artist": "1K Phew, WHATUPRG, Ty Brasel"
+  },
+  {
+    "id": "10",
+    "url": "../../music/parabolic.mp3",
+    "time": "02:49",
+    "title": "PARABOLIC!",
+    "artist": "nobigdyl."
+  },
+  {
+    "id": "11",
+    "url": "../../music/stix.mp3",
+    "time": "01:59",
+    "title": "STIX",
+    "artist": "nobigdyl."
+  },
+  {
+    "id": "12",
+    "url": "../../music/the scotts.mp3",
+    "time": "03:00",
+    "title": "THE SCOTTS",
+    "artist": "THE SCOTTS, Travis Scott, Kid Cudi"
+  },
+];
